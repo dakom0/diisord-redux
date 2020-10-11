2254af42485d75d780bedbd56f2d421ff8166d31
DAMI KOMOLAFE <damik94@gmail.com>
2020-10-10 21:54:48 -0400

    commit chat

--- src/ChatHeader.js
+++ src/ChatHeader.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import "./ChatHeader.css"
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import EditLocationRoundedIcon from '@material-ui/icons/EditLocationRounded';
+import PeopleAltRoundedIcon from '@material-ui/icons/PeopleAltRounded';
+import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
+import SendRoundedIcon from '@material-ui/icons/SendRounded';
+import HelpRoundedIcon from '@material-ui/icons/HelpRounded';
+
 function ChatHeader() {
     return (
         <div className="chatHeader">
-            <h3>I am the header</h3>
             <div className="chatHeader__left">
                 <h3>
                     <span className="chatHeader__hash">
@@ -11,6 +17,20 @@ function ChatHeader() {
                     </span>
                     Test Channel Name
                 </h3>
+            </div>
+
+            <div className="chatHeader__right">
+                <NotificationsIcon />
+                <EditLocationRoundedIcon />
+                <PeopleAltRoundedIcon />
+
+                <div className="chatHeader__search">
+                    <input placeholder="Search"/>
+                    <SearchRoundedIcon />
+                </div>
+
+                <SendRoundedIcon />
+                <HelpRoundedIcon />
             </div>
         </div>
     )
